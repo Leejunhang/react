bb8cbc29dc3de27c12ae02fa6a406868072da1b2
Leejunhang <share97@naver.com>
2023-10-25 17:03:14 +0900

    ver05

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import BookSearch from './components/ex03/BookSearch';
+import RouterPage from './components/ex03/RouterPage';
 
 const App = () =>{
 
     return (
         <div className="App">
-            <BookSearch/>
-            <hr/>
-            <BookSearch/>
+                <RouterPage/>
         </div>
     );
 }
