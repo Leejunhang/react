8f6eeff0a306d727b4f576c40a5d4f77152b8a46
Leejunhang <share97@naver.com>
2023-10-31 13:21:36 +0900

    ver17

--- back/routes/books.js
+++ back/routes/books.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var multer = require('multer');
+
+//도서이미지 업로드
+var upload = multer({
+	storage: multer.diskStorage({
+		destination:(req, file, done)=>{
+			done(null, './public/upload/book')
+		},
+		filename:(req, file, done)=> {
+			var fileName=Date.now() + '.jpg'
+			done(null, fileName);
+		}
+	})
+});
+
+//도서 이미지 업로드
+router.post('/update/image', upload.single('file'), function(req, res){
+    const filename='/upload/book/' + req.file.filename;
+    const bid=req.body.bid;
+    const sql='update books set image=?, regdate=now() where bid=?';
+    db.get().query(sql, [filename, bid], function(err){
+        if(err){
+            res.send('0');
+        }else{
+            res.send('1');
+        }
+    });
+});
 
 //도서목록
 router.get('/list.json', function(req, res){ //localhost:5000/books/list.json?query=&page=1&size=5
@@ -64,4 +92,21 @@ router.get('/read/:bid', function(req, res){    //localhost:5000/books/read/70
     });
 });
 
+//도서정보 수정
+router.post('/update', function(req, res){
+    const bid=req.body.bid;
+    const title=req.body.title;
+    const price=req.body.price;
+    const authors=req.body.authors;
+    const publisher=req.body.publisher;
+    const contents=req.body.contents;
+    const sql='update books set title=?, price=?, authors=?, publisher=?, contents=?, regdate=now() where bid=?'
+    db.get().query(sql,[title, price, authors, publisher, contents, bid], function(err){
+        if(err){
+            res.send('0');
+        }else{
+            res.send('1');
+        }
+    });
+})
 module.exports = router;
